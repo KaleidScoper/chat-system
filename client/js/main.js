6d4002d51ddba8c128d5e4af9bd8e50df4e4df1c
Kaleid Scoper <68382243+KaleidScoper@users.noreply.github.com>
2024-12-30 21:59:17 +0800

    Update main.js

--- client/js/main.js
+++ client/js/main.js
@@ -3,6 +3,7 @@ document.getElementById("login-btn").addEventListener("click", async () => {
     const password = document.getElementById("password").value;
 
     const response = await fetch("https://testgames.me/chat-system/server/api/login.php", {
+        // 此处使用相对路径有问题，不知道怎么回事
         method: "POST",
         body: JSON.stringify({ username, password }),
         headers: { "Content-Type": "application/json" },
@@ -12,6 +13,6 @@ document.getElementById("login-btn").addEventListener("click", async () => {
     if (result.status === "success") {
         window.location.href = "chat.html";
     } else {
-        alert("登录失败！");
+        alert("用户名或密码错误！");
     }
 });
