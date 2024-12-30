53579b4717f07f356ad9f1ab04f08661aca972b3
Kaleid Scoper <68382243+KaleidScoper@users.noreply.github.com>
2024-12-30 21:56:43 +0800

    Update main.js

--- client/js/main.js
+++ client/js/main.js
@@ -2,7 +2,7 @@ document.getElementById("login-btn").addEventListener("click", async () => {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    const response = await fetch("testgames.me/chat-system/server/api/login.php", {
+    const response = await fetch("https://testgames.me/chat-system/server/api/login.php", {
         method: "POST",
         body: JSON.stringify({ username, password }),
         headers: { "Content-Type": "application/json" },
